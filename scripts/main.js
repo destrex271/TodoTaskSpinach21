5e4a95ad31bdfd36b511149325a6c9d251dbb5cc
Akshat Jaimini <destrex271@gmail.com>
2022-04-22 21:09:09 +0530

    New

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,22 @@ const list = document.querySelector("#list")
 
 console.log("Hello")
 
-btn.addEventListener("click",e=>{
-    e.preventDefault()
-    val = field.value
+// Api Call
+
+const key = "UuB0Zfa5enuVL_JmlIHgZK8YwyMT1lUiPvg7tWxAumA"
+const apiEndpoint = `https://api.unsplash.com//photos/random/?client_id=${key}`
+
+fetch(apiEndpoint)
+.then(async (response)=>{
+    const res = await response.json()
+    const y = res["urls"]["full"]
+    document.body.style.backgroundImage = `url(${y})`
+})
+.catch(()=>console.log("Error"))
+
+// ----------
+
+const addItem = val => {
     list.innerHTML += `<li class="item col-sm-4 col-md-4 col-lg-4 col-xl-4">
     <div class="">${val}</div>
     <div class="del">
@@ -15,5 +28,22 @@ btn.addEventListener("click",e=>{
         <i class="fa fa-plus btn" style="color:yellow;font-size:25px;" aria-hidden="true"></i>
     </div>
     </li>`
+}
+
+let data = localStorage.getItem("data") || ""
+if(data){
+    const tl = data.split(",")
+    console.log(tl)
+    tl.forEach(elem => {
+        addItem(elem)
+    });
+}
+
+btn.addEventListener("click",e=>{
+    e.preventDefault()
+    val = field.value
+    data += `,${val}`
+    addItem(val)
     field.value = ""
+    localStorage.setItem("data",data)
 })
