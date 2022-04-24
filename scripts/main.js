a15807332e40fd1aacdc2becc74e1ab3ef31cb70
Akshat Jaimini <destrex271@gmail.com>
2022-04-24 21:02:02 +0530

    Fetch Enabled

--- scripts/main.js
+++ scripts/main.js
@@ -10,34 +10,35 @@ console.log("Hello")
 const key = "UuB0Zfa5enuVL_JmlIHgZK8YwyMT1lUiPvg7tWxAumA"
 const apiEndpoint = `https://api.unsplash.com//photos/random/?client_id=${key}`
 
-// fetch(apiEndpoint)
-// .then(async (response)=>{
-//     const res = await response.json()
-//     const y = res["urls"]["full"]
-//     document.body.style.backgroundImage = `url(${y})`
-// })
-// .catch(()=>console.log("Error"))
+fetch(apiEndpoint)
+.then(async (response)=>{
+    const res = await response.json()
+    const y = res["urls"]["full"]
+    document.body.style.backgroundImage = `url(${y})`
+})
+.catch(()=>console.log("Error"))
 
 // ----------
+
+let data = localStorage.getItem("data") || []
+if(typeof data == 'string') data = data.split(',')
+let item = 0
 
 list.addEventListener('click',(e)=>{
     const id = e.target.id
     console.log(e.target)
     if(id.includes("del-")){
         const y = parseInt(id.split("-")[1])
-        let x = data.split(",")
-        x[parseInt(y)] = ""
-        x = x.filter(elem=>elem.length != 0)
-        localStorage.setItem("data",x.toString())
+        data.splice(y,1)
         list.removeChild(document.getElementById(`${y}`))
+        localStorage.setItem("data",data.toString())
     }
     else if(id.includes("upd-")){
         const y = parseInt(id.split("-")[1])
         const response = prompt("New Value for Item?")
         if(response.length != 0){
-            let x = localStorage.getItem("data").split(",")
-            x[parseInt(y)] = response
-            localStorage.setItem("data",x.toString())
+            data[parseInt(y)] = response
+            localStorage.setItem("data",data.toString())
             document.location.reload()
         }else{
             alert("Empty Values not allowed!")
@@ -57,11 +58,8 @@ const addItem = (val) => {
     item++;
 }
 
-let data = localStorage.getItem("data") || ""
-let item = 0
 if(data){
-    const tl = data.split(",")
-    tl.forEach(elem => {
+    data.forEach(elem => {
         if(elem.length != 0)
             addItem(elem)
     });
@@ -70,8 +68,8 @@ if(data){
 btn.addEventListener("click",e=>{
     e.preventDefault()
     val = field.value
-    data += `,${val}`
+    data.push(val)
     addItem(val)
     field.value = ""
-    localStorage.setItem("data",data.split(",").filter(e=>e.length != 0).toString())
+    localStorage.setItem("data",data.toString())
 })
